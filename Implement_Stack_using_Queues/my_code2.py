f3b469f4ec815ea2921bc4c2c4cc1a0e75d085d2
Olena Kulynyak <kulynyakolena@gmail.com>
2026-04-07 22:22:52 +0300

    Add Stack using Queues solution

---
+++ Implement_Stack_using_Queues/my_code2.py
@@ -0,0 +1,34 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop(0)
+
+    def peek(self):
+        return self.items[0]
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+
+class MyStack:
+    def __init__(self):
+        self.queue = []
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+        for _ in range(len(self.queue) - 1):
+            self.queue.append(self.queue.pop(0))
+
+    def pop(self) -> int:
+        return self.queue.pop(0)
+
+    def top(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return len(self.queue) == 0
